a492e69c18898335ded6878d72844a0c35ae109b
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-07-13 21:46:01 +0530

    Updates

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,4 @@
-const users   =require("../models/user.js");
+const users = require("../models/user.js");
 
 /*
 Post request json file structure
@@ -17,9 +17,29 @@ Post request json file structure
 //you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
 //to look the user schema look ../models/user.js
 
-const registerUser =async (req, res) => {
+const registerUser = async (req, res) => {
 
     //Write you code here
+    try {
+        const { name, email, password } = req.body;
+        if (!name) return res.status(400).send("Name is required.");
+        if (!email) return res.status(400).send("Email is required.");
+        if (!password) return res.status(400).send("Password is required.")
+
+        const user = new users({
+            name: name,
+            email: email,
+            password: password
+        })
+        const responseFromDB = await user.save();
+        return res.send(responseFromDB._id);
+
+    } catch (error) {
+        return res.json({
+            status: 404,
+            message: error.message
+        })
+    }
 
 }
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ const fs = require("fs");
 
 dotenv.config();
 
-//connect to DB
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
+// //connect to DB
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
 
 const url = process.env.DATABASE_URL || "mongodb://0.0.0.0:27017/users";
-mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log('connected to DB')
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("DB Connected.")
+}).catch((error) => {
+    console.log(error)
 })
 
 
